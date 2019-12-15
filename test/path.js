66b4d215b3b957552b0fe4df7e13f0326af3919f
ehmicky <ehmicky@gmail.com>
2019-12-15 21:51:22 +0100

    Remove comment

--- test/path.js
+++ test/path.js
@@ -55,12 +55,6 @@ if (platform !== 'win32' || !isCi) {
   })
 }
 
-// `nyc nve ...` does not work because `nyc` monkey patches
-// `child_process.spawn()` and forces `node` to be `process.execPath` (the
-// `node` that spawned `nyc`, i.e. the global `node`) by modifying the `$PATH`
-// environment variable.
-// This should be fixed with nyc@15
-// See https://github.com/istanbuljs/spawn-wrap/issues/108
 test('Works with nyc as parent with node command', async t => {
   const { stdout } = await execa.command(`nyc --silent -- node ${DEEP_FILE}`)
 
