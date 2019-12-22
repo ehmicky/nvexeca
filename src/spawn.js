d4d43fff35fb6f142ee31902a02751f3d3f86369
ehmicky <ehmicky@gmail.com>
2019-12-22 23:00:05 +0100

    Update comment

--- src/spawn.js
+++ src/spawn.js
@@ -1,9 +1,9 @@
 import { npm } from 'global-dirs'
 
-// Some libraries like `spawn-wrap` (used by `nyc`) monkey patch
-// `child_process.spawn()` to modify `$PATH` and prepend their own `node`
-// wrapper. We fix it by using the `node` absolute path instead of relying on
-// `$PATH`. Note that this does not work:
+// Some libraries like `spawn-wrap` monkey patch `child_process.spawn()` to
+// modify `$PATH` and prepend their own `node` wrapper. We fix it by using the
+// `node` absolute path instead of relying on `$PATH`.
+// Note that this does not work:
 //  - with nested child processes
 //  - with binaries
 // This is also slightly faster as it does not require any `$PATH` lookup.
