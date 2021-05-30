1d2fb49aa22ee05d4b0cd27bfbcb3821c6f70673
ehmicky <ehmicky@gmail.com>
2021-05-30 15:56:24 +0200

    Move files

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import getNode from 'get-node'
 
 import { copyBinaries } from './copy/main.js'
 import { getOpts } from './options.js'
-import { getCommand, getExecaOptions } from './spawn.js'
 
 // Forwards command to another node instance of a specific `versionRange`
 // eslint-disable-next-line max-params
@@ -27,7 +26,6 @@ const nvexeca = async function (versionRange, command, args, opts) {
 
   const commandA = getCommand(nodePath, command)
   const execaOptionsB = getExecaOptions(nodePath, execaOptionsA)
-
   const childProcess = startProcess({
     command: commandA,
     args: argsA,
@@ -43,6 +41,28 @@ const nvexeca = async function (versionRange, command, args, opts) {
     args: argsA,
     execaOptions: execaOptionsB,
   }
+}
+
+// Some libraries like `spawn-wrap` monkey patch `child_process.spawn()` to
+// modify `$PATH` and prepend their own `node` wrapper. We fix it by using the
+// `node` absolute path instead of relying on `$PATH`.
+// Note that this does not work:
+//  - with nested child processes
+//  - with binaries
+// This is also slightly faster as it does not require any `$PATH` lookup.
+const getCommand = function (nodePath, command) {
+  return command === 'node' ? nodePath : command
+}
+
+// Forward arguments to another node binary located at `nodePath`.
+// Fix `$PATH` so that `node` points to the right version.
+// We do this instead of directly calling `node` so that:
+//  - child processes use the same Node.js version
+//  - binaries work, even on Windows
+// We use `execa` `execPath` for this.
+// This option requires `preferLocal: true`
+const getExecaOptions = function (nodePath, execaOptions) {
+  return { ...execaOptions, execPath: nodePath, preferLocal: true }
 }
 
 const startProcess = function ({ command, args, execaOptions, dry }) {
