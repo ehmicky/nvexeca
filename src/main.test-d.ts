a2a64c4e8816da1d23b2ed530a184e82cf3e76fe
ehmicky <ehmicky@gmail.com>
2022-12-04 20:09:13 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,64 +1,77 @@
 import type { Stream } from 'stream'
 
-import {
-  expectType,
-  expectError,
-  expectAssignable,
-  expectNotAssignable,
-} from 'tsd'
+import { expectType, expectAssignable, expectNotAssignable } from 'tsd'
 
 import nvexeca, { Options, ProcessInfo } from 'nvexeca'
 
 await nvexeca('14', 'echo')
-expectError(nvexeca())
-expectError(nvexeca('14'))
-expectError(nvexeca(14, 'echo'))
-expectError(nvexeca('14', true))
+// @ts-expect-error
+nvexeca()
+// @ts-expect-error
+nvexeca('14')
+// @ts-expect-error
+nvexeca(14, 'echo')
+// @ts-expect-error
+nvexeca('14', true)
 
 await nvexeca('14', 'echo', [])
 await nvexeca('14', 'echo', ['arg'])
-expectError(nvexeca('14', 'echo', true))
-expectError(nvexeca('14', 'echo', [true]))
+// @ts-expect-error
+nvexeca('14', 'echo', true)
+// @ts-expect-error
+nvexeca('14', 'echo', [true])
 
 await nvexeca('14', 'echo', {})
 await nvexeca('14', 'echo', [], {})
 await nvexeca('14', 'echo', ['arg'], {})
 expectAssignable<Options>({})
-expectError(await nvexeca('14', 'echo', true))
-expectError(await nvexeca('14', 'echo', [], true))
-expectError(await nvexeca('14', 'echo', { unknown: true }))
-expectError(await nvexeca('14', 'echo', [], { unknown: true }))
+// @ts-expect-error
+await nvexeca('14', 'echo', true)
+// @ts-expect-error
+await nvexeca('14', 'echo', [], true)
+// @ts-expect-error
+await nvexeca('14', 'echo', { unknown: true })
+// @ts-expect-error
+await nvexeca('14', 'echo', [], { unknown: true })
 
 await nvexeca('14', 'echo', { dry: true })
 expectAssignable<Options>({ dry: true })
-expectError(await nvexeca('14', 'echo', { dry: 'true' }))
+// @ts-expect-error
+await nvexeca('14', 'echo', { dry: 'true' })
 
 await nvexeca('14', 'echo', { progress: true })
 expectAssignable<Options>({ progress: true })
-expectError(await nvexeca('14', 'echo', { progress: 'true' }))
+// @ts-expect-error
+await nvexeca('14', 'echo', { progress: 'true' })
 
 await nvexeca('14', 'echo', { mirror: 'https://example.com' })
 expectAssignable<Options>({ mirror: 'https://example.com' })
-expectError(await nvexeca('14', 'echo', { mirror: true }))
+// @ts-expect-error
+await nvexeca('14', 'echo', { mirror: true })
 
 await nvexeca('14', 'echo', { fetch: true })
 await nvexeca('14', 'echo', { fetch: undefined })
 expectAssignable<Options>({ fetch: true })
-expectError(nvexeca('14', 'echo', { fetch: 'true' }))
+// @ts-expect-error
+nvexeca('14', 'echo', { fetch: 'true' })
 
 await nvexeca('14', 'echo', { arch: 'x64' })
 expectAssignable<Options>({ arch: 'x64' })
-expectError(await nvexeca('14', 'echo', { arch: true }))
-expectError(await nvexeca('14', 'echo', { arch: 'unknownArch' }))
+// @ts-expect-error
+await nvexeca('14', 'echo', { arch: true })
+// @ts-expect-error
+await nvexeca('14', 'echo', { arch: 'unknownArch' })
 
 await nvexeca('14', 'echo', { cwd: '.' })
 expectAssignable<Options>({ cwd: '.' })
 expectAssignable<Options>({ cwd: new URL('file://example.com') })
-expectError(await nvexeca('14', 'echo', { cwd: true }))
+// @ts-expect-error
+await nvexeca('14', 'echo', { cwd: true })
 
 await nvexeca('14', 'echo', { stripFinalNewline: true })
 expectAssignable<Options>({ stripFinalNewline: true })
-expectError(await nvexeca('14', 'echo', { stripFinalNewline: 'true' }))
+// @ts-expect-error
+await nvexeca('14', 'echo', { stripFinalNewline: 'true' })
 
 const processInfo = await nvexeca('14', 'echo')
 expectType<ProcessInfo>(processInfo)
