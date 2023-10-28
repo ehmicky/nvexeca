6ad14842e165c37a9aafc7423a7dc3d396528707
ehmicky <ehmicky@gmail.com>
2023-10-28 05:33:53 +0100

    Drop support for Node 16

--- src/helpers/versions.test.js
+++ src/helpers/versions.test.js
@@ -1,4 +1,4 @@
 export const TEST_VERSION = '6.2.1'
-export const HELPER_VERSION = '16.17.0'
+export const HELPER_VERSION = '18.18.0'
 export const ALIAS_VERSION = 'lts'
 export const INVALID_VERSION = 'invalid_version'
