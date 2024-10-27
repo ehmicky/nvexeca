174b0ffd6a2fd9c047bcbf5b8989fc8ff7d3fccf
ehmicky <ehmicky@gmail.com>
2024-10-27 10:20:03 +0000

    Fix Ava config

--- ava.config.js
+++ ava.config.js
@@ -1,1 +1,9 @@
-export { default } from '@ehmicky/dev-tasks/ava.config.js'
+import avaConfig from '@ehmicky/dev-tasks/ava.config.js'
+import isCI from 'is-ci'
+
+export default {
+  ...avaConfig,
+  // CI machines have lower limits for parallel network requests, and sometimes
+  // fail, especially macOS on GitHub actions
+  serial: isCI,
+}
